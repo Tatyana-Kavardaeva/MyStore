43786e63411a4935f40c004701f96d1671c98e7e
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-07-28 18:09:29 +0300

    update settings.py configuring the database mystore

--- config/settings.py
+++ config/settings.py
@@ -52,8 +52,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mystore',
+        'USER': 'postgres',
+        'PASSWORD': 'Nfnjxrf1989!'
     }
 }
 
