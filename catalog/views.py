bf5fa658758164ff42ef33a0afae3f4234da9e35
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-08-04 16:21:50 +0300

    update views.py add products_list, products_detail

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from catalog.models import Product
 
 
-def home(request):
-    return render(request, 'main/home.html')
+def base(request):
+    return render(request, 'main/base.html')
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'main/products_list.html', context)
+
+
+def products_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'main/products_detail.html', context)
 
 
 def contact(request):
