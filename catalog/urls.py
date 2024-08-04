b91b46431265d9a2df186d0765203cc72024d333
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-08-04 16:22:31 +0300

    update urls.py add path for products_list, products_detail

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from catalog.views import home, contact
 
+from catalog.apps import CatalogConfig
+from catalog.views import contact, products_detail, products_list
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', products_list, name='products_list'),
+    path('products/<int:pk>/', products_detail, name='products_detail'),
     path('contact/', contact, name='contact'),
 ]
