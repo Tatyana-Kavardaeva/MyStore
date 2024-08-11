07ecaadf908dd543fe6072af3e041cb3110ec5a5
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-08-11 23:16:21 +0300

    update blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Название')
+    body = models.TextField(verbose_name='Содержание')
+    image = models.ImageField(
+        upload_to="products/image",
+        blank=True,
+        null=True,
+        verbose_name="Изображение",
+        help_text="Загрузите изображение"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата создания",
+        help_text="Введите дату"
+    )
+    view_counter = models.PositiveIntegerField(
+        verbose_name="Просмотры",
+        help_text="Укажите количество просмотров",
+        default=0
+    )
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    slug = models.CharField(max_length=150, verbose_name='slug')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
