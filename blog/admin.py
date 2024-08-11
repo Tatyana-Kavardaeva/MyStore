21e5d154fa0f418221922d39c597d9d8630a58c5
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-08-11 23:19:31 +0300

    update blog/admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Blog
+
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+    list_filter = ('created_at', 'is_published')
+    search_fields = ('name', 'descriptions')
