096d5371254da144c9368172aa7cf9ba84dd6d29
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-08-11 23:16:48 +0300

    update config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -7,4 +7,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('catalog.urls', 'catalog'), namespace='catalog')),
+    path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
