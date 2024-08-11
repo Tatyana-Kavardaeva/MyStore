4146b117b2b289922ad2bc0153bd4739e31475a5
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-08-11 10:25:44 +0300

    create dir templates/catalog

--- catalog/views.py
+++ catalog/views.py
@@ -3,19 +3,19 @@ from catalog.models import Product
 
 
 def base(request):
-    return render(request, 'main/base.html')
+    return render(request, 'main/templates/catalog/base.html')
 
 
 def products_list(request):
     products = Product.objects.all()
     context = {'products': products}
-    return render(request, 'main/products_list.html', context)
+    return render(request, 'main/templates/catalog/products_list.html', context)
 
 
 def products_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     context = {'product': product}
-    return render(request, 'main/products_detail.html', context)
+    return render(request, 'main/templates/catalog/products_detail.html', context)
 
 
 def contact(request):
@@ -24,4 +24,4 @@ def contact(request):
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         print(f'You have new message from {name}({phone}): {message}')
-    return render(request, 'main/contact.html')
+    return render(request, 'main/templates/catalog/contact.html')
