b82f99c3b01978b480c2f8d742ae79f379cfe4e0
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-08-26 17:01:52 +0300

    update catalog\views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,46 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView, TemplateView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, TemplateView, CreateView, DeleteView, UpdateView
 
+from catalog.form import ProductForm
 from catalog.models import Product
 
 
 class ProductListView(ListView):
     model = Product
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        products = context['object_list']
+        for product in products:
+            active_version = product.version.filter(
+                indication=True).first()
+            product.active_version = active_version
+        return context
+
 
 class ProductDetailView(DetailView):
     model = Product
+
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:products_list')
+
+
+class ProductUpdateView(UpdateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy('catalog:product_detail')
+
+    def get_success_url(self):
+        return reverse('catalog:product_detail', args=[self.kwargs.get('pk')])
+
+
+class ProductDeleteView(DeleteView):
+    model = Product
+    success_url = reverse_lazy('catalog:products_list')
 
 
 class ContactPageView(TemplateView):
