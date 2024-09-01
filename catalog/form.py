c6d40ee527da2f06c2e6c89f3f2da36a753db0d5
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-09-01 16:58:01 +0300

    updated catalog/form.py exclude field owner

--- catalog/form.py
+++ catalog/form.py
@@ -16,7 +16,8 @@ class StyleFormMixin:
 class ProductForm(StyleFormMixin, ModelForm):
     class Meta:
         model = Product
-        fields = '__all__'
+        exclude = ('owner',)
+        # fields = '__all__'
 
     def clean_name(self):
         cleaned_data = self.cleaned_data.get('name')
