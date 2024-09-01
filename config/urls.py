33ab08fec8913a995a647a3c755a917a7d01014c
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-09-01 16:52:01 +0300

    updated catalog/urls.py added path for users

--- config/urls.py
+++ config/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('catalog.urls', 'catalog'), namespace='catalog')),
     path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('users/', include(('users.urls', 'users'), namespace='users'))
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
