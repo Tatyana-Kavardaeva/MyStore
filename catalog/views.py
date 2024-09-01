7ea210e2b4fb1eab4e69e6aaac7894449544cd06
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-09-01 16:54:40 +0300

    updated catalog/views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.forms import inlineformset_factory
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
@@ -24,7 +25,7 @@ class ProductDetailView(DetailView):
     model = Product
 
 
-class ProductCreateView(CreateView):
+class ProductCreateView(CreateView, LoginRequiredMixin):
     model = Product
     form_class = ProductForm
     success_url = reverse_lazy('catalog:products_list')
@@ -44,6 +45,11 @@ class ProductCreateView(CreateView):
         if formset.is_valid():
             formset.instance = self.object
             formset.save()
+
+        product = form.save()
+        user = self.request.user
+        product.owner = user
+        product.save()
 
         return super().form_valid(form)
 
