597189fba86c49ef9556d81de77039ded2933091
Tatyana Kavardaeva <solarpoint@yandex.ru>
2024-09-12 14:52:58 +0300

    added catalog\services.py

---
+++ catalog/services.py
@@ -0,0 +1,15 @@
+from catalog.models import Category
+from django.core.cache import cache
+from config.settings import CACHE_ENABLED
+
+
+def get_category_from_cache():
+    if not CACHE_ENABLED:
+        return Category.objects.all()
+    key = "category_list"
+    category = cache.get(key)
+    if category is not None:
+        return category
+    category = Category.objects.all()
+    cache.set(key, category)
+    return category
